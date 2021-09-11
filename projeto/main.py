acc755d3039763bca37e4dc17cdfbde9af42b6b2
NathanRibeiroC <nathanazevedo@gec.inatel.br>
2021-09-11 18:52:06 -0300

    feat: funcionalidade de edição de texto automatizada

---
+++ projeto/main.py
@@ -0,0 +1,11 @@
+from utils.video_utils import EdicaoVideo
+import os
+
+if __name__ == '__main__':
+    
+    # quem passa esse path é a interface do usuário
+    ev = EdicaoVideo("C:\\Users\\Nathan Ribeiro\\Desktop\\Nacional-URU 2 x 6 River Plate _ Melhores Momentos _ Libertadores 17_12_2020.mp4")
+    ev.edita_video()
+    
+    
+    
